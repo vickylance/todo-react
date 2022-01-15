b7934133cf5ccf83478540eb7151fea16f0ebdc5
Vignesh S <vignesh.s1@thoughtworks.com>
2022-01-15 20:25:35 +0530

    Added basic logic

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [todo, setTodo] = useState();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>Todo</div>
+      <input onChange={(e) => setTodo(e.target.value)} value={todo} />
+      <button
+        onClick={() => {
+          setTodos((prevList) => {
+            return [...prevList, todo];
+          });
+        }}
+      >
+        Add Todo
+      </button>
+      <div>
+        {todos.map((t) => {
+          return <div>{t}</div>;
+        })}
+      </div>
     </div>
   );
 }
